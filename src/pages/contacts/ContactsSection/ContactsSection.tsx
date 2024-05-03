f0f9d426874b1d5924e5cba1ce081289b9db56cf
Grande <grand.meloyan@yandex.ru>
2024-05-04 02:13:14 +0500

    Добавлены контакты

--- src/pages/contacts/ContactsSection/ContactsSection.tsx
+++ src/pages/contacts/ContactsSection/ContactsSection.tsx
@@ -1,43 +1,37 @@
 import Maps from "../../../components/Maps/Maps.tsx";
-import InfoItem from "../../../components/InfoItem/InfoItem.tsx";
-import mapPin from "../../../assets/icons/map-pin.svg";
-import phone from "../../../assets/icons/phone.svg";
-import whatsapp from "../../../assets/icons/whatsapp.svg";
 
 export default function ContactsSection() {
     return (
-        <section className='flex-col lg:flex-row justify-between bg-pale-silver py-8 rounded-3xl'>
-            <h1 className='px-4 lg:px-8 xl:px-12 py-8 text-deep-purple-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold leading-none'>
+        <section className='flex-col lg:flex-row bg-pale-silver py-8 rounded-3xl'>
+            <h1 className='max-lg:text-center px-4 lg:px-8 xl:px-12 py-8 text-deep-purple-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold leading-none'>
                 Контакты
             </h1>
             <div
-                className='grid-cols-2 items-center lg:items-start text-center lg:text-left px-4 lg:px-8 xl:px-12 pb-8 lg:flex-1'>
+                className='grid-cols-2 px-4 lg:px-8 xl:px-12 pb-8 lg:flex-1'>
                 <nav
-                    className='text-center lg:text-left px-4 lg:px-8 xl:px-12 py-8 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold leading-none bg-white rounded-3xl'>
-                    <InfoItem
-                        icon={mapPin}>
-                        <a href='https://yandex.ru/maps/-/CDVxAFpm' target='_blank'>
-                            г. Екатеринбург, ул. Авиационная, д. 57
-                        </a>
-                    </InfoItem>
-                    <InfoItem icon={phone}>
-                    <a href='tel:+73432923331' target='_blank'>+7 (343) 292-33-31</a>
-                    </InfoItem>
-                    <InfoItem icon={phone}>
-                        <a href='tel:+79582349133' target='_blank'>+79582349133</a>
-                    </InfoItem>
-                    <InfoItem icon={whatsapp}>
-                        <a href='https://wa.me/79582349133' target='_blank'>+79582349133</a>
-                    </InfoItem>
-                    <InfoItem icon={phone}>
-                        <a href='mailto:ulybka.2021@bk.ru' target='_blank'>ulybka.2021@bk.ru</a>
-                    </InfoItem>
+                    className='px-4 lg:px-8 xl:px-12 py-8 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold leading-none bg-white rounded-3xl'>
+                    <div className='max-lg:text-center font-medium relative flex-col items-center text-black'>
+                        <p className='my-2'>
+                            Адрес: <a className='transition duration-500 hover:text-soft-beige cursor-pointer' href='https://yandex.ru/maps/-/CDVxAFpm' target='_blank'>г. Екатеринбург, ул. Авиационная, д. 57</a>
+                        </p>
+                        <p className='my-2'>
+                            Телефон: <a className='transition duration-500 hover:text-soft-beige cursor-pointer' href='tel:+73432923331' target='_blank'>+7 (343) 292-33-31</a> | <a className='transition duration-500 hover:text-soft-beige cursor-pointer' href='tel:+79582349133' target='_blank'>+79582349133</a>
+                        </p>
+                        <p className='my-2'>
+                            Whatsapp: <a className='transition duration-500 hover:text-soft-beige cursor-pointer' href='https://wa.me/79582349133' target='_blank'>+79582349133</a>
+                        </p>
+                        <p className='my-2'>
+                            Telegram: <a className='transition duration-500 hover:text-soft-beige cursor-pointer' href='https://t.me/gallerysmiles96' target='_blank'>@gallerysmiles96</a>
+                        </p>
+                        <p className='my-2'>
+                            E-mail: <a className='transition duration-500 hover:text-soft-beige cursor-pointer' href='mailto:ulybka.2021@bk.ru' target='_blank'>ulybka.2021@bk.ru</a>
+                        </p>
+                    </div>
                 </nav>
                 <div className='lg:flex-1 w-full rounded-3xl overflow-hidden'>
                     <Maps/>
                 </div>
             </div>
-
         </section>
     );
 }
--- src/pages/price-list/PricesHeroSection/PricesHeroSection.tsx
+++ src/pages/price-list/PricesHeroSection/PricesHeroSection.tsx
@@ -1,30 +1,46 @@
 import Button from '../../../components/Button/Button.tsx'
 import priceListBanner from '../../../assets/images/price-list-banner.png'
+import Maps from "../../../components/Maps/Maps.tsx";
 
 export default function PricesHeroSection() {
     return (
-        <section
-            className='flex flex-col-reverse lg:flex-row items-center lg:justify-between w-full bg-dark-green rounded-3xl'>
+        <section className='flex-col lg:flex-row bg-pale-silver py-8 rounded-3xl'>
+            <h1 className='max-lg:text-center px-4 lg:px-8 xl:px-12 py-8 text-deep-purple-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold leading-none'>
+                Контакты
+            </h1>
             <div
-                className='flex flex-col items-center lg:items-start text-center lg:text-left px-4 lg:px-8 xl:px-12 py-8 '>
-                <h1 className='text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold leading-none'>
-                    Прайс-лист
-                </h1>
-                <p className='text-white text-gray-600 text-base md:text-lg lg:text-xl mt-6 mb-8 md:my-12'>
-                    Наши цены вас приятно удивят!
-                </p>
-                <Button
-                    color="bg-deep-purple-black"
-                    link="#"
-                    className='inline-flex text-2xl hover:bg-black'
-                    buttonText="Записаться на приём"/>
-
-            </div>
-            <div className='w-full lg:max-w-lg xl:max-w-xl py-0 lg:p-0'>
-                <img src={priceListBanner} alt='Услуги' className='"w-full ml-36 my-2 w-2/3 p-4' loading="lazy"/>
-                <a href="https://www.freepik.com/free-psd/3d-illustration-stomatology-dentistry_36026353.htm#fromView=search&page=2&position=14&uuid=b61f45a7-d4b1-49d9-80dc-bf889b57c2ba"
-                   className='text-bright-blue text-transparent'>Image
-                    by freepik</a>
+                className='grid-cols-2 px-4 lg:px-8 xl:px-12 pb-8 lg:flex-1'>
+                <nav
+                    className='px-4 lg:px-8 xl:px-12 py-8 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold leading-none bg-white rounded-3xl'>
+                    <div className='max-lg:text-center font-medium relative flex-col items-center text-black'>
+                        <p className='my-2'>
+                            Адрес: <a className='transition duration-500 hover:text-soft-beige cursor-pointer'
+                                      href='https://yandex.ru/maps/-/CDVxAFpm' target='_blank'>г. Екатеринбург, ул.
+                            Авиационная, д. 57</a>
+                        </p>
+                        <p className='my-2'>
+                            Телефон: <a className='transition duration-500 hover:text-soft-beige cursor-pointer'
+                                        href='tel:+73432923331' target='_blank'>+7 (343) 292-33-31</a> | <a
+                            className='transition duration-500 hover:text-soft-beige cursor-pointer'
+                            href='tel:+79582349133' target='_blank'>+79582349133</a>
+                        </p>
+                        <p className='my-2'>
+                            Whatsapp: <a className='transition duration-500 hover:text-soft-beige cursor-pointer'
+                                         href='https://wa.me/79582349133' target='_blank'>+79582349133</a>
+                        </p>
+                        <p className='my-2'>
+                            Telegram: <a className='transition duration-500 hover:text-soft-beige cursor-pointer'
+                                         href='https://t.me/gallerysmiles96' target='_blank'>@gallerysmiles96</a>
+                        </p>
+                        <p className='my-2'>
+                            E-mail: <a className='transition duration-500 hover:text-soft-beige cursor-pointer'
+                                       href='mailto:ulybka.2021@bk.ru' target='_blank'>ulybka.2021@bk.ru</a>
+                        </p>
+                    </div>
+                </nav>
+                <div className='lg:flex-1 w-full rounded-3xl overflow-hidden'>
+                    <Maps/>
+                </div>
             </div>
         </section>
     );
